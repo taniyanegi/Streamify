85c587771e41ffa38a3a68738a4a68add5b81f5d
taniyanegi <taniyanegi632@gmail.com>
2026-06-18 22:15:45 +0530

    Update deployment configs for Vercel and Render

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,7 +55,7 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -87,7 +87,7 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       secure: process.env.NODE_ENV === "production",
     });
 
